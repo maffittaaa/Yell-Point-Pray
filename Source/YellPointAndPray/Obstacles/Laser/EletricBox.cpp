b787969f9e3383bafddb8d3e277a17df79a41fe3
Maffittaa <mafaldamartins2001@gmail.com>
2025-11-10 01:44:38 +0000

    started the eletric box

--- Source/YellPointAndPray/Obstacles/Laser/EletricBox.cpp
+++ Source/YellPointAndPray/Obstacles/Laser/EletricBox.cpp
@@ -1,24 +1,29 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Obstacles/Laser/EletricBox.h"
 
-// Sets default values
 AEletricBox::AEletricBox()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	eletricBoxMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Electric Box"));
+	eletricBoxMesh->SetupAttachment(RootComponent);
 
 }
 
-// Called when the game starts or when spawned
 void AEletricBox::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+void AEletricBox::Interact_Implementation(AActor* Interactor) {
+	for (ALaser* laser : lasers)
+		laser->niagaraLaserImpact->SetActive(false);
+
+	UE_LOG(LogTemp, Warning, TEXT("Laser is deactivated"));
+}
+
 void AEletricBox::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/YellPointAndPray/Obstacles/Laser/EletricBox.h
+++ Source/YellPointAndPray/Obstacles/Laser/EletricBox.h
@@ -4,10 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interfaces/Interactable.h"
+#include "Obstacles/Laser/Laser.h"
 #include "EletricBox.generated.h"
 
 UCLASS()
-class YELLPOINTANDPRAY_API AEletricBox : public AActor
+class YELLPOINTANDPRAY_API AEletricBox : public AActor, public IInteractable
 {
 	GENERATED_BODY()
 	
@@ -15,6 +17,12 @@ public:
 	// Sets default values for this actor's properties
 	AEletricBox();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* eletricBoxMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Laser")
+	TArray<ALaser*> lasers;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,5 +30,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	virtual void Interact_Implementation(AActor* interactor) override;
 
 };
