cddfeb4e6e30e98364fa1117a3ace238696496eb
artugatuga <tomas.m.lopes15@gmail.com>
2025-11-12 16:00:49 +0000

    Update MatchmakingSubsystem.cpp

--- Source/YellPointAndPray/Server/MatchmakingSubsystem.cpp
+++ Source/YellPointAndPray/Server/MatchmakingSubsystem.cpp
@@ -19,7 +19,7 @@ void UMatchmakingSubsystem::Deinitialize() {
 void UMatchmakingSubsystem::ConnectToMatchmakingServer() {
 	if (ClientRunnable) return; // already running
 
-	FString ServerIP = TEXT("10.208.208.87");
+	FString ServerIP = TEXT("10.208.208.68");
 
 	ClientRunnable = new FTCPClientRunnable(this, ServerIP);
 	UE_LOG(LogTemp, Warning, TEXT("ConnectToMatchmakingServer to %s"), *ServerIP);
