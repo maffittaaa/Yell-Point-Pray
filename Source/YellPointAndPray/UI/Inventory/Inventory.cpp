ea03600d9cebc1df52768537165e6d94d4d973a5
artugatuga <148588412+artugatuga@users.noreply.github.com>
2025-12-16 17:32:08 +0000

    Update Inventory.cpp

--- Source/YellPointAndPray/UI/Inventory/Inventory.cpp
+++ Source/YellPointAndPray/UI/Inventory/Inventory.cpp
@@ -34,8 +34,10 @@ void UInventory::StoreInitialInventory(TArray<FUInventoryStruct> InitialInventor
 void UInventory::RestoreInventoryWithTravelData(const TArray<FUInventoryStruct>& TravelData)
 {
 	// Clear current inventory
+	UE_LOG(LogTemp, Warning, TEXT("Start Restoring inventory"));
 	for (int i = 0; i < InventorySlots.Num(); i++)
 	{
+		UE_LOG(LogTemp, Warning, TEXT("First Item: %d"), i);
 		if (InventorySlots[i].Item != nullptr)
 		{
 			FDetachmentTransformRules DetachRules(EDetachmentRule::KeepWorld, true);
@@ -45,9 +47,12 @@ void UInventory::RestoreInventoryWithTravelData(const TArray<FUInventoryStruct>&
 		ResetSlotToDefaultValue(i);
 	}
 
+	UE_LOG(LogTemp, Warning, TEXT("First Round Over"));
+
 	// Restore from travel data
 	for (int i = 0; i < TravelData.Num() && i < InventorySlots.Num(); i++)
 	{
+		UE_LOG(LogTemp, Warning, TEXT("Second Item: %d"), i);
 		InventorySlots[i] = TravelData[i];
 	}
 
